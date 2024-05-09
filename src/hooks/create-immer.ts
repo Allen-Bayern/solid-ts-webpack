7edd5af7824f9d991dfaeb3c0293a706a82187da
yueb <zhaozhengyang_byn@pku.edu.cn>
2024-05-10 07:39:50 +0800

    perf: createImmer

--- src/hooks/create-immer.ts
+++ src/hooks/create-immer.ts
@@ -3,15 +3,18 @@ import { produce, freeze as immerFreeze } from 'immer';
 
 type Updater<T> = (draft: T) => void;
 
-/** @description translate `useImmer` (React) to `crateImmer` for updating object  */
+/**
+ * @description translate a hook used by `React` -- `useImmer`,  to `createImmer` for `Solid.js`
+ * @param v the value
+ */
 export function createImmer<T = unknown>(v: T) {
     const [state, setState] = createSignal(immerFreeze(v));
 
     const updater = (updater: T | Updater<T>) => {
         if (typeof updater === 'function') {
-            setState(() => produce(state(), updater as Updater<T>));
+            setState(oldState => produce(oldState, updater as Updater<T>));
         } else {
-            setState(() => updater);
+            setState(oldState => (updater === oldState ? oldState : updater));
         }
     };
 
