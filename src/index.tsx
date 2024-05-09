6a08258da0771bcf4313e67bee5287ffa373f9f5
yueb <zhaozhengyang_byn@pku.edu.cn>
2024-05-10 07:52:55 +0800

    perf: if '#app' is not exist, render to body

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import { render } from 'solid-js/web';
 import App from './App';
 
-const injected = document.querySelector('#app');
-if (injected) {
-    render(() => <App />, injected);
+/** @description main function */
+function main() {
+    const dom = document.getElementById('app');
+    const injectedDom = dom ? dom : document.body;
+
+    return render(() => <App />, injectedDom);
 }
+
+main();
