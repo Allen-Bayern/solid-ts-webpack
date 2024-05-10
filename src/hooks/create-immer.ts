aee6d2df44fad932034df9e8a806659ef0432e13
yueb <zhaozhengyang_byn@pku.edu.cn>
2024-05-11 06:35:51 +0800

    perf: shallowEqual function

--- src/hooks/create-immer.ts
+++ src/hooks/create-immer.ts
@@ -1,21 +1,63 @@
 import { createSignal, type SignalOptions } from 'solid-js';
-import { isEqual } from 'lodash';
+import { isEqual, uniq } from 'lodash';
 import { produce, freeze as immerFreeze } from 'immer';
 
 type Updater<T> = (draft: T) => void;
 
-interface CreateImmerOptions<T = unknown> extends SignalOptions<T> {
-    isDeep?: boolean;
-}
-
 /** @description Rewrite the equal function of number */
-const numberEqual = <T extends number = number>(num1: T, num2: T): boolean => {
+const numberEqual = (num1: number, num2: number): boolean => {
     if (Number.isNaN(num1) && Number.isNaN(num2)) {
         return true;
     }
 
     return num1 === num2;
 };
+
+/** @description shallow equal comparator inspired by React */
+const shallowEqual = <T>(prev: T, nextValue: T): boolean => {
+    if (typeof prev === 'number' && typeof nextValue === 'number') {
+        return numberEqual(prev, nextValue);
+    }
+
+    if (typeof prev === 'object' && typeof nextValue === 'object') {
+        if (prev !== null && nextValue !== null) {
+            const prevKeys = [...Object.keys(prev), ...Object.getOwnPropertySymbols(prev)];
+            const nextKeys = [...Object.keys(nextValue), ...Object.getOwnPropertySymbols(nextValue)];
+
+            const publicKeys = uniq([...prevKeys, ...nextKeys]);
+            if (publicKeys.length !== prevKeys.length || prevKeys.length !== nextKeys.length) {
+                return false;
+            }
+
+            let res = true;
+
+            // iter the keys
+            for (const currentKey of publicKeys) {
+                const currentPrev = prev[currentKey as keyof T];
+                const currentNext = nextValue[currentKey as keyof T];
+
+                if (typeof currentPrev === 'number' && typeof currentNext === 'number') {
+                    res = numberEqual(currentPrev, currentNext);
+                } else {
+                    res = Object.is(currentPrev, currentNext);
+                }
+
+                // If `false` is met, break the iteration.
+                if (!res) {
+                    break;
+                }
+            }
+
+            return res;
+        }
+    }
+
+    return Object.is(prev, nextValue);
+};
+
+interface CreateImmerOptions<T = unknown> extends SignalOptions<T> {
+    isDeep?: boolean;
+}
 
 /**
  * @description translate a hook used by `React` -- `useImmer`,  to `createImmer` for `Solid.js`
@@ -29,48 +71,7 @@ export function createImmer<T = unknown>(
 ) {
     const { isDeep = false, ...restOpts } = opts;
 
-    /** @description shallow equal comparator inspired by React */
-    const shallowEqual = (prev: T, nextValue: T) => {
-        if (typeof prev === 'number' && typeof nextValue === 'number') {
-            return numberEqual(prev, nextValue);
-        }
-
-        if (typeof prev === 'object' && typeof nextValue === 'object') {
-            if (prev !== null && nextValue !== null) {
-                const prevKeys = [...Object.keys(prev), ...Object.getOwnPropertySymbols(prev)];
-                const nextKeys = [...Object.keys(nextValue), ...Object.getOwnPropertySymbols(nextValue)];
-
-                const publicKeys = Array.from(new Set([...prevKeys, ...nextKeys]));
-                if (publicKeys.length !== prevKeys.length || prevKeys.length !== nextKeys.length) {
-                    return false;
-                }
-
-                let res = true;
-
-                for (const currentKey of publicKeys) {
-                    const currentPrev = prev[currentKey as keyof T];
-                    const currentNext = nextValue[currentKey as keyof T];
-
-                    if (typeof currentPrev === 'number' && typeof currentNext === 'number') {
-                        res = numberEqual(currentPrev, currentNext);
-                    } else {
-                        res = Object.is(currentPrev, currentNext);
-                    }
-
-                    if (!res) {
-                        break;
-                    }
-                }
-
-                return res;
-            }
-        }
-
-        return Object.is(prev, nextValue);
-    };
-
     const { equals: selfEquals } = restOpts;
-
     /** @description to decide which equal function to take */
     const getEquals = () => {
         if (selfEquals) {
