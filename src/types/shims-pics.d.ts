c17cd7cc98478525fd994257c311dd095c12c165
yueb <zhaozhengyang_byn@pku.edu.cn>
2024-05-11 22:59:23 +0800

    feat: add shims-pics

---
+++ src/types/shims-pics.d.ts
@@ -0,0 +1,24 @@
+declare module '*.jpg' {
+    const jpg: string;
+    export default jpg;
+}
+
+declare module '*.jpeg' {
+    const jpeg: string;
+    export default jpeg;
+}
+
+declare module '*.png' {
+    const png: string;
+    export default png;
+}
+
+declare module '*.gif' {
+    const gif: string;
+    export default gif;
+}
+
+declare module '*.ico' {
+    const ico: string;
+    export default ico;
+}
